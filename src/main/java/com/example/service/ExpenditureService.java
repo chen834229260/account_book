b0ebbb8713e644bd7051165cbac09be6cd542151
chen834229260 <834229260@qq.com>
2021-01-15 10:23:55 +0800

    日期调整

--- src/main/java/com/example/service/ExpenditureService.java
+++ src/main/java/com/example/service/ExpenditureService.java
@@ -11,8 +11,10 @@ import org.springframework.stereotype.Service;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.time.temporal.TemporalAdjusters;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -25,6 +27,7 @@ import java.util.stream.Collectors;
 public class ExpenditureService extends ServiceImpl<ExpenditureMapper, ExpenditureVO> {
     /**
      * 添加
+     *
      * @param expenditureVO
      */
     public Boolean addExpenditure(ExpenditureVO expenditureVO) {
@@ -37,20 +40,22 @@ public class ExpenditureService extends ServiceImpl<ExpenditureMapper, Expenditu
     public List<Double> monthlyExpenditureStatistics() throws ParseException {
         List<ExpenditureVO> list = this.list();
         List<Double> resultTotalList = new ArrayList<>();
-        int currentMonth = LocalDate.now().getMonthValue();
-        for (int i = 13; i > 1; i--) {
-            int temp = currentMonth  - i - 10;
-            if (temp > currentMonth) {
-                temp -= temp * 2;
-            }
-            String monthStart = LocalDate.now().plusMonths(temp).with(TemporalAdjusters.firstDayOfMonth()) + " 00:00:00";
-            String monthEnd = LocalDate.now().plusMonths(temp).with(TemporalAdjusters.lastDayOfMonth()) + " 23:59:59";
-            SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-            Long monthStartTime = format.parse(monthStart).getTime() / 1000L;
-            Long monthEndTime = format.parse(monthEnd).getTime() / 1000L;
-            List<ExpenditureVO> resultList = list.stream().filter(s -> s.getAddTime() <= monthEndTime && s.getAddTime() >= monthStartTime).collect(Collectors.toList());
+        for (int i = 1; i <= 12; i++) {
+            String time = i >= 10 ? "2021-" + i + "-01T00:00:00" : "2021-0" + i + "-01T00:00:00";
+            LocalDateTime today = LocalDateTime.parse(time);
+            //本月的第一天
+            LocalDate firstDay = LocalDate.of(today.getYear(), today.getMonth(), 1);
+            LocalDateTime lastDay = today.with(TemporalAdjusters.lastDayOfMonth());
+            String start = firstDay + " 00:00:00";
+            String end = lastDay.toLocalDate() + " 23:59:59";
+            SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+            Date startDate = simpleDateFormat.parse(start);
+            Date endDate = simpleDateFormat.parse(end);
+            Long startTime = startDate.getTime() / 1000L;
+            Long endTime = endDate.getTime() / 1000L;
+            List<ExpenditureVO> resultList = list.stream().filter(s -> s.getAddTime() <= endTime && s.getAddTime() >= startTime).collect(Collectors.toList());
             Double total = 0.0;
-            for (ExpenditureVO sv:resultList){
+            for (ExpenditureVO sv : resultList) {
                 total += sv.getMoney();
             }
             resultTotalList.add(total);
